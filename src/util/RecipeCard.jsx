7b7dd0bef5def6074bf2f50a7ca2892cb00087b6
Cambria Sullivan <72573258+cambsull@users.noreply.github.com>
2024-08-16 22:39:50 -0700

    Several CSS changes, color scheme changes

--- src/util/RecipeCard.jsx
+++ src/util/RecipeCard.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import './RecipeCard.css';
 
-function RecipeCard({ recipe, complexity, dishType, totalTime, ingredientCount}) {
+function RecipeCard({ recipe, complexity, dishType, totalTime, ingredientCount }) {
 
-    function handleButtonClick() {
-        window.open(recipe.url, '_blank');
-    };
+  function handleButtonClick() {
+    window.open(recipe.url, '_blank');
+  };
 
   return (
     <div className="recipe-card">
       <img src={recipe.image} alt={recipe.label} className="recipe-image" />
       <h2>{recipe.label}</h2>
-      <p><b>Dish type</b>: {dishType}</p>
-      <p><b>Ingredient count</b>: {ingredientCount}</p>
-      <p><b>Time to prepare</b>: {totalTime}</p>
-      <p><b>Complexity</b>: {complexity}</p>
+      <div className="recipeInfoContainer">
+        <p><b>Dish type</b>: {dishType}</p>
+        <p><b>Ingredient count</b>: {ingredientCount}</p>
+        <p><b>Time to prepare</b>: {totalTime}</p>
+        <p><b>Complexity</b>: {complexity}</p>
+      </div>
 
       <button onClick={handleButtonClick}>
         View Recipe
